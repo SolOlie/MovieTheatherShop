634b103b6f4a00ceb760a8332e4744b285a46ff4
agmam <ag-mam@live.dk>
2016-09-19 13:44:04 +0200

    Imanager created

---
+++ MovieTheaterShop/MovieTheaterShopDll/IManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieTheaterShopDll
+{
+    public interface IManager<T>
+    {
+       T Create(T t);
+       void Delete(T t);
+
+       T Update(T t);
+
+       List<T> ReadAll();
+
+    }
+}
