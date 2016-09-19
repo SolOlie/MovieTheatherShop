3074231f67295f321fc2490ab008db139b3ceafb
EsbenLaursen <Esben.laursen@gmail.com>
2016-09-19 14:10:19 +0200

    jfefi

---
+++ MovieTheaterShop/MovieShopAdmin/DAL/MovieShopContext.cs
@@ -0,0 +1,27 @@
+﻿using MovieTheaterShopDll.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MovieShopAdmin.DAL
+{
+    public class MovieShopContext : DbContext
+    {
+        public MovieShopContext() : base()
+        {
+            
+        }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Customer> Customer { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public Address Addresses { get; set; }
+
+
+
+
+
+    }
+}
